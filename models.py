ba6ddd0d026f33e225f855229f08c25acaf99a2f
Mitul Mistry <MitulMistryDev@gmail.com>
2021-06-09 22:56:00 -0500

    Add relationships and updates to models

--- models.py
+++ models.py
@@ -1,4 +1,8 @@
 from app import db
+from datetime import datetime
+
+# Models used to translate into database schema using Flask-SQLAlchemy (ORM)
+# User has many Holdings, and User has many Transactions (One-to-Many relationships)
 
 class User(db.Model):
     # Set table name in database (override default)
@@ -7,7 +11,10 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    cash = db.Column(db.Integer, nullable=False)
+    password_hash = db.Column(db.String, nullable=False)
+    cash = db.Column(db.Integer, nullable=False, default=10000)
+    holdings = db.relationship("holdings", backref="user", lazy=True)
+    transactions = db.relationship("transactions", backref="user", lazy=True)
 
     # Method to represent the object when we query for it
     def __repr__(self):
@@ -17,8 +24,9 @@ class Holding(db.Model):
     __tablename__ = "holdings"
 
     id = db.Column(db.Integer, primary_key=True)
-    symbol = db.Column(db.String(6), nullable=False)
+    symbol = db.Column(db.String(5), nullable=False)
     shares = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable="False")
 
     def __repr__(self):
         return "<Holding %r>" % self.symbol
@@ -27,10 +35,11 @@ class Transaction(db.Model):
     __tablename__ = "transactions"
 
     id = db.Column(db.Integer, primary_key=True)
-    symbol = db.Column(db.String(6), nullable=False)
+    symbol = db.Column(db.String(5), nullable=False)
     shares = db.Column(db.Float, nullable=False)
     price = db.Column(db.Float, nullable=False)
-    timestamp = db.Column(db.DateTime, nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable="False")
 
     def __repr__(self):
         return "<Transaction %r>" % self.timestamp
